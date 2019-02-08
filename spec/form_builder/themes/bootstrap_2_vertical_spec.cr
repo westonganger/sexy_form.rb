df0609c9d7167844a43ac2c3416cac3d0fd27e07
Weston Ganger <weston@westonganger.com>
2019-02-07 23:00:56 -0800

    Implement Theme :bootstrap_2_vertical

---
+++ spec/form_builder/themes/bootstrap_2_vertical_spec.cr
@@ -0,0 +1,97 @@
+require "../../spec_helper"
+require "./theme_spec_helper"
+
+theme_klass = FormBuilder::Themes::Bootstrap2Horizontal
+theme = theme_klass.new
+
+describe theme_klass do
+
+  describe ".theme_name" do
+    it "is correct" do
+      theme_klass.theme_name.should eq("bootstrap_2_horizontal")
+    end
+  end
+
+  describe ".wrap_field" do
+    it "works" do
+
+    end
+  end
+
+  describe "FormBuilder.form" do
+    it "matches bootstrap 2 docs example" do
+      expected = String.build do |str|
+        str << %(<form method="post">)
+          str << %(<div class="control-group">)
+            str << %(<label class="control-label" for="inputEmail">Email</label>)
+            str << %(<div class="controls">)
+              str << %(<input type="text" id="inputEmail" placeholder="Email">)
+            str << %(</div>)
+          str << %(</div>)
+          str << %(<div class="control-group">)
+            str << %(<label class="control-label" for="inputPassword">Password</label>)
+            str << %(<div class="controls">)
+              str << %(<input type="password" id="inputPassword" placeholder="Password">)
+           str << %(</div>)
+           str << %(</div>)
+          str << %(<div class="control-group">)
+            str << %(<div class="controls">)
+              str << %(<label class="checkbox">)
+                str << %(<input type="checkbox"> Remember me)
+              str << %(</label>)
+            str << %(</div>)
+          str << %(</div>)
+          str << %(<button type="submit" class="btn">Sign in</button>)
+        str << %(</form>)
+      end
+
+      actual = FormBuilder.form(theme: :bootstrap_2_vertical) do |f|
+        f << f.field(type: :text, label: "Email", input_html: {id: "inputEmail", placeholder: "Email"})
+        f << f.field(type: :password, label: "Password", input_html: {id: "inputPassword", placeholder: "Password"})
+        f << f.field(type: :checkbox, label: "Remember me")
+        f << %(<button type="submit" class="btn">Sign in</button>)
+      end
+
+      actual.should eq(expected)
+    end
+  end
+
+  describe ".input_html_attributes" do
+    FIELD_TYPES.each do |field_type|
+      it "returns correct #{field_type} attributes" do
+        attrs = StringHash.new
+
+        theme.input_html_attributes(html_attrs: StringHash.new, field_type: field_type, label_text: "Foobar").should eq(attrs)
+      end
+    end
+  end
+
+  describe ".label_html_attributes" do
+    FIELD_TYPES.each do |field_type|
+      it "returns correct #{field_type} attributes" do
+        attrs = StringHash.new
+
+        if {"checkbox", "radio"}.includes?(field_type)
+          attrs["class"] = field_type
+        else
+          attrs["class"] = "control-label"
+        end
+
+        attrs["for"] = "foobar"
+
+        theme.label_html_attributes(html_attrs: {"for" => "foobar"}, field_type: field_type).should eq(attrs)
+      end
+    end
+  end
+
+  describe ".form_html_attributes" do
+    it "returns correct attributes" do
+      attrs = StringHash.new
+
+      attrs["class"] = "form-horizontal"
+
+      theme.form_html_attributes(html_attrs: StringHash.new).should eq(attrs)
+    end
+  end
+
+end
--- src/form_builder/themes/bootstrap_2_vertical.cr
+++ src/form_builder/themes/bootstrap_2_vertical.cr
@@ -8,12 +8,25 @@ module FormBuilder
 
       def wrap_field(field_type : String, html_label : String?, html_field : String, field_errors : Array(String)?, wrapper_html_attributes : StringHash)
         String.build do |s|
-          if {"checkbox", "radio"}.includes?(field_type) && html_label && (i = html_label.index(">"))
-            s << "#{html_label.insert(i+1, "#{html_field} ")}"
+          s << %(<div class="control-group">)
+
+          if {"checkbox", "radio"}.includes?(field_type)
+            s << %(<div class="controls">)
+
+            if html_label && (i = html_label.index(">"))
+              s << "#{html_label.insert(i+1, "#{html_field} ")}"
+            else
+              s << html_label
+              s << html_field
+            end
+
+            s << "</div>"
           else
             s << html_label
-            s << html_field
+            s << %(<div class="controls">#{html_field}</div>)
           end
+
+          s << "</div>"
         end
       end
 
@@ -22,17 +35,21 @@ module FormBuilder
       end
 
       def label_html_attributes(html_attrs : StringHash, field_type : String, name : String? = nil, label_text : String? = nil)
+        html_attrs["class"] ||= ""
+
         if {"checkbox", "radio"}.includes?(field_type)
-          html_attrs["class"] = "#{html_attrs["class"]?} #{field_type}".strip
+          html_attrs["class"] = "#{html_attrs["class"]} #{field_type}".strip
+        else
+          html_attrs["class"] = "#{html_attrs["class"]} control-label".strip
         end
 
         html_attrs
       end
 
       def form_html_attributes(html_attrs : StringHash)
-        html_attrs["class"] = "#{html_attrs["class"]?} form-vertical".strip
         html_attrs
       end
+
 
     end
   end
