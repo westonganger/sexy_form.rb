0882449c8fbf5b1c0a8aa786c933e4d6675c77af
Weston Ganger <weston@westonganger.com>
2019-02-09 22:48:16 -0800

    Implement Theme :bulma_horizontal

---
+++ spec/form_builder/themes/bulma_horizontal_spec.cr
@@ -0,0 +1,108 @@
+require "../../spec_helper"
+require "./theme_spec_helper"
+
+theme_klass = FormBuilder::Themes::BulmaHorizontal
+theme = theme_klass.new
+
+describe theme_klass do
+
+  describe ".theme_name" do
+    it "is correct" do
+      theme_klass.theme_name.should eq("bulma_horizontal")
+    end
+  end
+
+  describe ".wrap_field" do
+    it "works" do
+
+    end
+  end
+
+  describe "FormBuilder.form" do
+    it "matches docs example with labels" do
+      expected = String.build do |str|
+        str << %(<form method="post">)
+          str << %(<div class="field is-horizontal">)
+            str << %(<label class="label is-normal" for="email">Email</label>)
+            str << %(<div class="field-body">)
+              str << %(<div class="field">)
+                str << %(<div class="control">)
+                  str << %(<input type="text" name="email" id="email">)
+                str << "</div>"
+              str << "</div>"
+            str << "</div>"
+          str << "</div>"
+
+          str << %(<div class="field is-horizontal">)
+            str << %(<label class="label is-normal" for="password">Password</label>)
+            str << %(<div class="field-body">)
+              str << %(<div class="field">)
+                str << %(<div class="control">)
+                  str << %(<input type="password" name="password" id="password">)
+                str << "</div>"
+              str << "</div>"
+            str << "</div>"
+          str << "</div>"
+
+          str << %(<div class="field is-horizontal">)
+            str << %(<div class="field-body">)
+              str << %(<div class="field">)
+                str << %(<div class="control">)
+                  str << %(<label class="checkbox" for="remember_me">)
+                    str << %(<input type="checkbox" name="remember_me" id="remember_me"> Remember Me)
+                  str << %(</label>)
+                str << "</div>"
+              str << "</div>"
+            str << "</div>"
+          str << "</div>"
+
+          str << %(<button type="submit" class="button">Sign in</button>)
+        str <<%(</form>)
+      end
+
+      actual = FormBuilder.form(theme: theme_klass.theme_name) do |f|
+        f << f.field(type: :text, name: :email)
+        f << f.field(type: :password, name: :password)
+        f << f.field(type: :checkbox, name: :remember_me)
+        f << %(<button type="submit" class="button">Sign in</button>)
+      end
+
+      actual.should eq(expected)
+    end
+  end
+
+  describe ".input_html_attributes" do
+    FIELD_TYPES.each do |field_type|
+      it "returns correct #{field_type} attributes" do
+        attrs = StringHash.new
+
+        theme.input_html_attributes(html_attrs: StringHash.new, field_type: field_type).should eq(attrs)
+      end
+    end
+  end
+
+  describe ".label_html_attributes" do
+    FIELD_TYPES.each do |field_type|
+      it "returns correct #{field_type} attributes" do
+        attrs = StringHash.new
+
+        if {"checkbox", "radio"}.includes?(field_type)
+          attrs["class"] = field_type
+        else
+          attrs["class"] = "label is-normal"
+        end
+
+        theme.label_html_attributes(html_attrs: StringHash.new, field_type: field_type).should eq(attrs)
+      end
+    end
+  end
+
+  describe ".form_html_attributes" do
+    it "returns correct attributes" do
+      attrs = StringHash.new
+
+      theme.form_html_attributes(html_attrs: StringHash.new).should eq(attrs)
+    end
+  end
+
+end
--- src/form_builder/themes/bulma_horizontal.cr
+++ src/form_builder/themes/bulma_horizontal.cr
@@ -4,16 +4,28 @@ module FormBuilder
 
       def wrap_field(field_type : String, html_label : String?, html_field : String, field_errors : Array(String)?, wrapper_html_attributes : StringHash)
         String.build do |s|
+          wrapper_html_attributes["class"] = "field is-horizontal #{wrapper_html_attributes["class"]?}".strip
+
           attr_str = build_html_attr_string(wrapper_html_attributes)
           s << "#{attr_str.empty? ? "<div>" : %(<div #{attr_str}>)}"
 
-          if {"checkbox", "radio"}.includes?(field_type) && html_label && (i = html_label.index(">"))
-            s << "#{html_label.insert(i+1, "#{html_field} ")}"
-          else
+          unless {"checkbox", "radio"}.includes?(field_type) && html_label && (i = html_label.index(">"))
             s << html_label
+          end
+
+          s << %(<div class="field-body">)
+          s << %(<div class="field">)
+          s << %(<div class="control">)
+
+          if {"checkbox", "radio"}.includes?(field_type) && html_label && (i = html_label.index(">"))
+            s << html_label.insert(i+1, "#{html_field} ")
+          else
             s << html_field
           end
 
+          s << "</div>"
+          s << "</div>"
+          s << "</div>"
           s << "</div>"
         end
       end
@@ -25,13 +37,13 @@ module FormBuilder
       def label_html_attributes(html_attrs : StringHash, field_type : String)
         if {"checkbox", "radio"}.includes?(field_type)
           html_attrs["class"] = "#{field_type} #{html_attrs["class"]?}".strip
+        else
+          html_attrs["class"] = "label is-normal #{html_attrs["class"]?}".strip
         end
-
         html_attrs
       end
 
       def form_html_attributes(html_attrs : StringHash)
-        html_attrs["class"] = "form-inline #{html_attrs["class"]?}".strip
         html_attrs
       end
 
