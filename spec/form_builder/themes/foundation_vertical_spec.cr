b2c86e099f79092103b1c4fc4b7271c61047c9a9
Weston Ganger <weston@westonganger.com>
2019-02-09 23:24:13 -0800

    Implement Theme :foundation

---
+++ spec/form_builder/themes/foundation_vertical_spec.cr
@@ -0,0 +1,85 @@
+require "../../spec_helper"
+require "./theme_spec_helper"
+
+theme_klass = FormBuilder::Themes::FoundationVertical
+theme = theme_klass.new
+
+describe theme_klass do
+
+  describe ".theme_name" do
+    it "is correct" do
+      theme_klass.theme_name.should eq("foundation_vertical")
+    end
+  end
+
+  describe ".wrap_field" do
+    it "works" do
+
+    end
+  end
+
+  describe "FormBuilder.form" do
+    it "matches docs example with labels" do
+      expected = String.build do |str|
+        str << %(<form method="post">)
+          str << %(<div>)
+            str << %(<label for="email">Email)
+            str << %(<input type="text" name="email" id="email">)
+            str << "</label>"
+          str << "</div>"
+
+          str << %(<div>)
+            str << %(<label for="password">Password)
+            str << %(<input type="password" name="password" id="password">)
+            str << "</label>"
+          str << "</div>"
+
+          str << %(<div>)
+            str << %(<input type="checkbox" name="remember_me" id="remember_me">)
+            str << %(<label for="remember_me">Remember Me</label>)
+          str << "</div>"
+
+          str << %(<button type="submit">Sign in</button>)
+        str <<%(</form>)
+      end
+
+      actual = FormBuilder.form(theme: theme_klass.theme_name) do |f|
+        f << f.field(type: :text, name: :email)
+        f << f.field(type: :password, name: :password)
+        f << f.field(type: :checkbox, name: :remember_me)
+        f << %(<button type="submit">Sign in</button>)
+      end
+
+      actual.should eq(expected)
+    end
+  end
+
+  describe ".input_html_attributes" do
+    FIELD_TYPES.each do |field_type|
+      it "returns correct #{field_type} attributes" do
+        attrs = StringHash.new
+
+        theme.input_html_attributes(html_attrs: StringHash.new, field_type: field_type).should eq(attrs)
+      end
+    end
+  end
+
+  describe ".label_html_attributes" do
+    FIELD_TYPES.each do |field_type|
+      it "returns correct #{field_type} attributes" do
+        attrs = StringHash.new
+
+        theme.label_html_attributes(html_attrs: StringHash.new, field_type: field_type).should eq(attrs)
+      end
+    end
+  end
+
+  describe ".form_html_attributes" do
+    it "returns correct attributes" do
+      attrs = StringHash.new
+
+      theme.form_html_attributes(html_attrs: StringHash.new).should eq(attrs)
+    end
+  end
+
+end
--- src/form_builder/themes/foundation_vertical.cr
+++ src/form_builder/themes/foundation_vertical.cr
@@ -3,7 +3,19 @@ module FormBuilder
     class FoundationVertical < Themes
 
       def wrap_field(field_type : String, html_label : String?, html_field : String, field_errors : Array(String)?, wrapper_html_attributes : StringHash)
-        "Foo to the Bar"
+        String.build do |s|
+          attr_str = build_html_attr_string(wrapper_html_attributes)
+          s << "#{attr_str.empty? ? "<div>" : %(<div #{attr_str}>)}"
+
+          if !{"checkbox", "radio"}.includes?(field_type) && html_label
+            s << html_label.sub("</label>", "#{html_field}</label>")
+          else
+            s << html_field
+            s << html_label
+          end
+
+          s << "</div>"
+        end
       end
 
       def input_html_attributes(html_attrs : StringHash, field_type : String)
